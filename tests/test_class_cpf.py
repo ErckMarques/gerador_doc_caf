d568107137b16b0df9cdbf39ea4f47d4ff1b2860
Erik Marques <lucroalternativo@outlook.com>
2025-05-23 13:21:55 -0300

    testes

--- tests/test_class_cpf.py
+++ tests/test_class_cpf.py
@@ -4,13 +4,18 @@ from gerador_docs import CPF
 from gerador_docs.errors import CPFInvalidError
 
 @pytest.mark.parametrize(
-    "cpf_input",
+    "cpf_repeted_numbers",
     [
         "11111111111",
         "22222222222",
         "33333333333",
     ]
 )
+def test_cpf_repetd_numbers(cpf_repeted_numbers):
+    with pytest.raises(CPFInvalidError) as exc_info:
+        CPF(cpf_repeted_numbers)
+        assert str(exc_info.value) == f"Erro O CPF não pode conter todos os dígitos iguais."
+
 @pytest.mark.parametrize(
     "cpf_lengh_invalid",
     [
@@ -18,6 +23,12 @@ from gerador_docs.errors import CPFInvalidError
         "1234568791564765"
     ]
 )
+def test_cpf_invalid(cpf_lengh_invalid):
+    '''Testa se o cpf tem 11 dígitos.'''
+    with pytest.raises(CPFInvalidError) as exc_info:
+        CPF(cpf_lengh_invalid)
+        assert str(exc_info.value) == f"O CPF deve ter 11 dígitos."
+
 @pytest.mark.parametrize(
     "cpf_format_invalid",
     [
@@ -25,15 +36,8 @@ from gerador_docs.errors import CPFInvalidError
         "1235as456sd",
     ]
 )
-def test_cpf_invalid(cpf_input, cpf_lengh_invalid, cpf_format_invalid):
-    with pytest.raises(CPFInvalidError) as exc_info:
-        CPF(cpf_input)
-        assert str(exc_info.value) == f"CPF inválido: {cpf_input}. Erro O CPF não pode conter todos os dígitos iguais."
-    
-    with pytest.raises(CPFInvalidError) as exc_info:
-        CPF(cpf_lengh_invalid)
-        assert str(exc_info.value) == f"O CPF deve ter 11 dígitos."
-
+def test_cpf_invalid(cpf_format_invalid):
+    '''Testa se o cpf contem apenas números.'''
     with pytest.raises(CPFInvalidError) as exc_info:
         CPF(cpf_format_invalid)
         assert str(exc_info.value) == f"O CPF deve conter apenas dígitos."
