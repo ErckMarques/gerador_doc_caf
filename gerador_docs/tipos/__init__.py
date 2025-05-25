68add65726d68a26d861a5d394892dc5d6506035
Erik Marques <lucro.alternativo@outlook.com>
2025-05-25 12:04:00 -0300

    refactored __init.py

---
+++ gerador_docs/tipos/__init__.py
@@ -0,0 +1,4 @@
+from gerador_docs.tipos.documents import CPF, RG
+from gerador_docs.tipos.endereco import Endereco
+from gerador_docs.tipos.dados_pessoais import DadosPessoais
+from gerador_docs.tipos._typing import EnderecoDict, DadosPessoaisDict
