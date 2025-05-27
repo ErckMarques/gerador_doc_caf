7c4569f458ad06262f8fc46172891e177c64d7db
Erik Marques <lucro.alternativo@outlook.com>
2025-05-26 21:02:13 -0300

    Adicionadas exceptions para as classes de dados CAR e CAF

--- gerador_docs/errors/__init__.py
+++ gerador_docs/errors/__init__.py
@@ -22,3 +22,26 @@ class MaritalStatusError(Exception):
     """Exception raised for invalid marital status."""
     pass
 
+class CARNumberFormatError(Exception):
+    """Exception raised for invalid car number format."""
+    pass
+
+class CARNumberLengthError(Exception):
+    """Exception raised for invalid car number length."""
+    pass
+
+class CARNumberInvalidError(Exception):
+    """Exception raised for invalid car number."""
+    pass
+
+class CAFNumberFormatError(Exception):
+    """Exception raised for invalid CAF number format."""
+    pass
+
+class CAFNumberLengthError(Exception):
+    """Exception raised for invalid CAF number length."""
+    pass
+
+class CAFNumberInvalidError(Exception):
+    """Exception raised for invalid CAF number."""
+    pass
