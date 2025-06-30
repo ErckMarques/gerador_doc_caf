183312551e3dbfb0b80af9e00fd36d64df9f5f73
Erik Marques <lucro.alternativo@outlook.com>
2025-06-29 23:25:08 -0300

    add method for the "db" command

--- gerador_docs/cli/runners.py
+++ gerador_docs/cli/runners.py
@@ -3,3 +3,6 @@ from argparse import Namespace
 class DefaultRunner:
     def poco(self, args: Namespace) -> None:
         print(f"Executando comando 'poco' com os argumentos: {args}")
+
+    def db(self, args: Namespace) -> None:
+        print(f"Executando comando 'db' com os argumentos: {args}")
