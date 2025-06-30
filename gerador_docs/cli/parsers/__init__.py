727e7318af860cfa4e6982eb5c5df7ad25e97399
Erik Marques <lucroalternativo@outlook.com>
2025-06-30 13:14:31 -0300

    added subparsers to the main parser and added structure for commands in the DefaultRunner class

--- gerador_docs/cli/parsers/__init__.py
+++ gerador_docs/cli/parsers/__init__.py
@@ -2,9 +2,15 @@ from argparse import ArgumentParser
 
 from .parser_db import db_subparser
 from .parser_pocos import poco_subparser
+from .parser_caf import caf_subparser
+from .parser_declaracao import declaracao_subparser
+from .parser_pag import pagamento_subparser
 
 def config_subparsers(parser: ArgumentParser) -> None: 
     subparser = parser.add_subparsers(dest="command", required=True,)
 
     db_subparser(subparser)
     poco_subparser(subparser)
+    caf_subparser(subparser)
+    declaracao_subparser(subparser)
+    pagamento_subparser(subparser)
--- gerador_docs/cli/runners.py
+++ gerador_docs/cli/runners.py
@@ -6,3 +6,13 @@ class DefaultRunner:
 
     def db(self, args: Namespace) -> None:
         print(f"Executando comando 'db' com os argumentos: {args}")
+
+    def dec(self, args: Namespace) -> None:
+        print(f"Executando comando 'dec' com os argumentos: {args}")
+    
+    def pagamento(self, args: Namespace) -> None:
+        print(f"Executando comando 'pagamento' com os argumentos: {args}")
+    
+    def caf(self, args: Namespace) -> None:
+        print(f"Executando comando 'caf' com os argumentos: {args}")
+
