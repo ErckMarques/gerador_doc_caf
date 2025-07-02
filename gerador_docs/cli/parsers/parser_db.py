2b7c61d1f2faaca61454a9f1339293506a274476
Erik Marques <lucro.alternativo@outlook.com>
2025-07-02 06:12:05 -0300

    db subparser refactored for better usability. Description and usage texts have also been improved. In the future use features from the rich lib.

--- gerador_docs/cli/parsers/parser_db.py
+++ gerador_docs/cli/parsers/parser_db.py
@@ -1,53 +1,79 @@
-from argparse import ArgumentParser, _SubParsersAction
-from argparse import ONE_OR_MORE
+from argparse import ArgumentParser, _SubParsersAction, ONE_OR_MORE
 
-from rich_argparse import RawDescriptionRichHelpFormatter
+import textwrap
 
-def db_subparser(subparser: _SubParsersAction) -> ArgumentParser:
-    db_subparser: ArgumentParser = subparser.add_parser(
+def db_subparser(subparser: _SubParsersAction, /, formatter_class) -> ArgumentParser:
+    db_parser: ArgumentParser = subparser.add_parser(
         "db",
-        formatter_class=RawDescriptionRichHelpFormatter,
+        formatter_class=formatter_class,
+        description=textwrap.dedent(
+            """
+            Use este comando para adicionar, remover, atualizar ou listar dados em tabelas específicas.
+            Os dados devem ser fornecidos como argumentos adicionais atravéa do argumento posicional '--dados',
+            e a ação a ser executada deve ser especificada pelo argumento [blue]OBRIGATÓRIO[/] '--action'.
+            
+            As ações disponíveis são:
+                - add: Adiciona novos dados à tabela.
+                - remove: Remove dados da tabela.
+                - update: Atualiza dados existentes na tabela.
+                - list: Lista os dados da tabela, ou os nomes das tabelas + descrição disponíveis (em estudo de viabilização).
+            Cada ação requer que você forneça os dados necessários através do argumento '--dados'.
+            
+            [yellow]IMPORTANTE: O argumento '--dados' pode ser usado múltiplas vezes para fornecer vários valores.[/]
+            
+            [red]ATENÇÃO[/]: 
+                ao utilizar [green]'--action remove'[/] -> [green]'--dados'[/] deve receber um identificador ou vários identificadores 
+                que permita identificar o dado no banco de dados. Em caso de dúvidas sobre o identificador utilize: [green]'--action list'[/].
+            """
+        ),
+        usage=textwrap.dedent(
+            """
+            Exemplos de uso:
+                1. Adicionar valores a uma tabela:
+                    - %(prog)s NOME_TABELA --dados VALOR1 --action add
+                    - %(prog)s NOME_TABELA --dados VALOR1 --dados VALOR2 --action add
+                2. Remover/Atualizar valores a uma tabela:
+                    - %(prog)s NOME_TABELA --dados VALOR1 --action remove
+                    - %(prog)s NOME_TABELA --dados VALOR1 --dados VALOR2 --action remove
+                    - %(prog)s NOME_TABELA --dados VALOR1 --dados VALOR2 --action update
+                3. Listar registros de uma tabela
+                    - %(prog)s NOME_TABELA --action list
+                4. Listar todas as tabelas disponiveis
+                    - %(prog)s --action list (em estudo de viabilização)
+            """
+        ),
         help="Comandos relacionados à manipulação dos dados dos poços e seus responsáveis (CRUD)."
     )
 
-    db_subparser.set_defaults(
-        command="db",
-    )
+    db_parser.set_defaults(command="db")
 
-    db_subparser.add_argument(
-        "-t", "--table",
+    # Argumento posicional obrigatório: nome da tabela
+    db_parser.add_argument(
+        "table",
+        default=None,
         help="Nome da tabela a ser manipulada",
-        required=True,
-        metavar="table name".upper(),
+        metavar="NOME_TABELA",
     )
 
-    db_subparser.add_argument(
-        "-a", "--add",
-        help="adiciona as informações de um poço",
+    # Argumento opcional repetível: dados
+    db_parser.add_argument(
+        "--dados",
+        help="Dados a serem manipulados. Pode ser usado múltiplas vezes.",
+        default=None,
         nargs=ONE_OR_MORE,
         action="append",
-        metavar="resp local ci".upper(),
+        metavar="VALORES"
     )
 
-    db_subparser.add_argument(
-        "-r", "--remove",
-        help="remove as informações de um poço",
-        nargs=ONE_OR_MORE,
-        metavar="id / ci".upper(),
-    )  
-
-    db_subparser.add_argument(
-        "-u", "--update",
-        help="atualiza as informações de um poço",
-        nargs=ONE_OR_MORE,
-        metavar="id / ci".upper(),
-    )
-    
-    # lista as informações de um poço
-    db_subparser.add_argument(
-        "-l", "--list",
-        metavar="id / ci".upper(),
-        help="lista as informações de um poço",
+    # Ação CRUD como uma única opção com choices
+    db_parser.add_argument(
+        "--action",
+        metavar='ACTION',
+        choices=["add", "remove", "update", "list"],
+        help="Ação a ser executada na tabela."
     )
 
-    return db_subparser
+    return db_parser
+
+# todo: função de validação dos dados para cada ação.
+# todo: add rich elements for terminal exibition
