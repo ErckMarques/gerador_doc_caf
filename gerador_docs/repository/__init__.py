8c6b912b379376dc4beabf85cad8a383ac0dc095
Erik Marques <lucroalternativo@outlook.com>
2025-07-04 13:37:49 -0300

    construção da estrutura e dos objetos do utilizados para interagir com os sgbd

--- gerador_docs/repository/__init__.py
+++ gerador_docs/repository/__init__.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from tinydb import TinyDB, Query
 from tinydb.table import Document
 
@@ -12,8 +14,29 @@ Contudo, percebi que antes de definir como vou fazer as coisas, preciso voltar e
 ou a forma como vou inserir dados apartir da CLI e as tabelas que serão utilizadas. 
 """
 
-def create_db():
-    db = TinyDB()
+_INSTANCE_PATH = Path(__file__).parent / 'instance'
+
+if not _INSTANCE_PATH.exists():
+    _INSTANCE_PATH.mkdir()
+
+def create_engine(db_path_or_uri: Path | str):
+    """Esta tem por proposito realizar configurações e instanciar o objeto de conexao com o banco de dados.
+    Args:
+        db_path_or_uri (Path | str): 
+            string ou objeto Path de caminho do banco ou arquivo de banco de dados ou nome do banco de dados, por padrão, 
+            se passado apenas 'meu_banco.json', ele é criado na raiz do projeto dentro de './instance/'.
+            ou string URI semelhante à utilizada no sqlalchemy, mysql ou postgressql.
+    
+            Ex.: db_path_or_uri='tinydb+aiotinydb://instance/dados.json'
+            Ex.: db_path_or_uri='sgbd+extension://instance/dados.json'
+
+    """
+    global _INSTANCE_PATH
+
+    def select_repository(): 
+        """Factory que retorna uma instancia de IRepository, especificada na URI de conexão, por padrão trabalha com o tinydb."""
+        pass
+
 
 
 class TinyDbRepository:
